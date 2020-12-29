be88220f2f32dd0cf13d0ef226f9dfe40e0cbbd5
User <User@DESKTOP-1HC7HGQ>
2020-12-29 19:09:04 +0900

    my sceond

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -3,7 +3,7 @@ package icehs.science.chapter02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!!!");
 
 	}
 
